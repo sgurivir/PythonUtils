2f0a00fada4f6fdff6259a9e938527b7a2e1eab0
Sridhar Gurivireddy <sgurivireddy@apple.com>
2022-01-26 21:28:21 -0800

    update

--- wordScrabble/BoardModel.py
+++ wordScrabble/BoardModel.py
@@ -26,6 +26,11 @@ class BoardModel:
         self.size = BoardModel.SIZE_OF_BOARD
         self.num_words = BoardModel.NUM_WORDS
 
+        self.start_index_for_row = {}
+        self.length_of_word_for_row = {}
+        self.start_index_for_column = {}
+        self.length_of_word_for_column = {}
+
     def dump(self):
         print("=====" * self.size)
         output = ""
@@ -44,24 +49,21 @@ class BoardModel:
         return sum_
 
     def find_boxes_to_fill(self):
-
         find_row_or_column = False  # true for row, false for column
         rows_filled = []
         columns_filled = []
-        start_index_for_row = {}
-        start_index_for_column = {}
 
         found_word_in_this_iteration = False
-        while (len(start_index_for_row.keys()) + len(start_index_for_column.keys())) < self.num_words:
+        while (len(self.start_index_for_row.keys()) + len(self.start_index_for_column.keys())) < self.num_words:
             length_of_word = random.randint(3, BoardModel.MAX_WORD_LENGTH)
 
             if found_word_in_this_iteration:
                 find_row_or_column = not find_row_or_column  # find row and column alternately
 
                 # Override if we found enough rows or columns
-                if len(start_index_for_row.keys()) == self.num_words/2:
+                if len(self.start_index_for_row.keys()) == self.num_words/2:
                     find_row_or_column = False
-                elif len(start_index_for_column.keys()) == self.num_words/2:
+                elif len(self.start_index_for_column.keys()) == self.num_words/2:
                     find_row_or_column = True
 
             if find_row_or_column is True:
@@ -69,7 +71,7 @@ class BoardModel:
                     if r % 2 != 0:
                         continue
 
-                    if start_index_for_row.keys().__contains__(r):
+                    if self.start_index_for_row.keys().__contains__(r):
                         continue
 
                     beg = r * self.size
@@ -86,7 +88,8 @@ class BoardModel:
                     self.tiles[beg + c: beg + c + length_of_word] = [1] * length_of_word
                     found_word_in_this_iteration = True
                     rows_filled.append(r)
-                    start_index_for_row[r] = c
+                    self.start_index_for_row[r] = c
+                    self.length_of_word_for_row[r] = length_of_word
                     break
 
             else:
@@ -97,7 +100,7 @@ class BoardModel:
                         c_ = c_ + 1
                     c = c_ % self.size
 
-                    if start_index_for_column.keys().__contains__(c):
+                    if self.start_index_for_column.keys().__contains__(c):
                         continue
 
                     r = random.randint(0, self.size - length_of_word)
@@ -112,11 +115,12 @@ class BoardModel:
                         found_word_in_this_iteration = True
 
                         columns_filled.append(c)
-                        start_index_for_column[c] = r
+                        self.start_index_for_column[c] = r
+                        self.length_of_word_for_column[c] = length_of_word
                         break
 
 
-def load_thesarus(path):
+def load_thesaurus(path):
     with open(thesaurus_path, 'rb') as t:
         all_words = pickle.load(t)
         words = {}
@@ -131,4 +135,5 @@ if __name__ == '__main__':
     board.dump()
 
     thesaurus_path = "/Users/sgurivireddy/my_src/PythonUtils/wordScrabble/output/Board_Dictionary"
-    words = load_thesarus(thesaurus_path)
+    words = load_thesaurus(thesaurus_path)
+
